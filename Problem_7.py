8195d7dabab566f41210d377280cf183eec2443d
a-carroll-1 <carroll.alex.d@gmail.com>
2018-12-12 13:07:38 -0700

    Update Problem_7.py

--- Problem_7.py
+++ Problem_7.py
@@ -3,3 +3,26 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 
 What is the 10 001st prime number?
 """
+import math
+import timeit
+
+tic = timeit.default_timer()
+def is_prime(num):
+    """ Return True if the number is Prime, False if it is composite. """
+    if num < 2:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+li = [2]
+i = 0
+while len(li) <= 10001:
+    if is_prime(i):
+        li.append(i)
+    i += 1
+
+print(li[-1])
+toc = timeit.default_timer()
+print(toc - tic)
